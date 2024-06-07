dd287d71fba8de5b6949f6a9b0cb97fa41aa0b77
Costa curta, Marcelo [CONBR NON-J&J] <MCostaCu@its.jnj.com>
2024-06-07 17:42:47 -0300

    feat: add opening schema and custom logger

---
+++ config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import "gorm.io/gorm"
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	return nil
+}
+
+func GetLogger(prefix string) *Logger {
+	logger = NewLogger(prefix)
+	return logger
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/mcostacurta/go-opportunities-api/router"
+import (
+	"github.com/mcostacurta/go-opportunities-api/config"
+	"github.com/mcostacurta/go-opportunities-api/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	router.Initialize() // listen and serve on 0.0.0.0:8080
 }
